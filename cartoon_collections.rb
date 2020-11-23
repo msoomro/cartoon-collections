2f400da11fd5828ec14f22ac824f59b6a59331e0
Moomal <moomalmsoomro@gmail.com>
2020-11-22 23:19:29 -0500

    commit tree 4e0bda8e1810f1b9a3f559f31160e4790b8fa0a9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,47 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# requires array of dwarf names
+# outputs each name in number order
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# requires array of planeteer calls
+# returns a modified array
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# requires an array of calls
+# returns true if any call is longer than four chars, else false
+def long_planeteer_calls(calls)
+  i = 0
+
+  while i < calls.size
+    if calls[i].length > 4
+      return true
+    end
+    i += 1
+  end
+
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# requires an array of strings
+# returns a string (name of cheese)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.each do |item|
+    cheese_types.each do |cheese|
+      if cheese == item
+        return cheese
+      end
+    end
+  end
+
+  nil
 end
+
